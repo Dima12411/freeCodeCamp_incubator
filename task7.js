119a435ce143511ff85dd86fd94abed90d164031
Dima12411 <fixer40delta@gmail.com>
2021-10-23 19:10:32 +0300

    solution of the task7

---
+++ task7.js
@@ -0,0 +1,17 @@
+/* Task:
+Modify the existing declarations and assignments so their names use camelCase.
+
+Do not create any new variables. */
+
+
+//code:
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
