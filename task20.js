5267c3ec18d6e67547fb21ae24b061f506555b50
Dima12411 <fixer40delta@gmail.com>
2021-10-24 18:30:05 +0300

    solution of the task20

---
+++ task20.js
@@ -0,0 +1,13 @@
+/* Task:
+Convert the assignments for a, b, and c to use the *= operator */
+
+//code:
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
