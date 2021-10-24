f633605d4899161461dbbc48754cc0d8ad5db316
Dima12411 <fixer40delta@gmail.com>
2021-10-24 18:32:30 +0300

    solution of the task21

---
+++ task21.js
@@ -0,0 +1,13 @@
+/* Task:
+Convert the assignments for a, b, and c to use the /= operator */
+
+//code:
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
