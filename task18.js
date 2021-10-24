fafeb897c708109ecafdeffac7c2f4c8498d4976
Dima12411 <fixer40delta@gmail.com>
2021-10-24 18:25:51 +0300

    solution of the task18

---
+++ task18.js
@@ -0,0 +1,13 @@
+/* Task:
+Convert the assignments for a, b, and c to use the += operator. */
+
+//code:
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7; 
