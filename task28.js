69629b728da92c5bac8a9de2d59a75639a7478bd
Dima12411 <fixer40delta@gmail.com>
2021-10-24 19:11:32 +0300

    solution of the task28

---
+++ task28.js
@@ -0,0 +1,7 @@
+/* Task:
+Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well! */
+
+//code:
+
+var myName = "Dima";
+var myStr = "My name is " + myName + " and I am well!";
