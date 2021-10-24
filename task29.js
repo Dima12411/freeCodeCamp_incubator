fdce316aa7dfc46cd0ca8a2a0af7a17ecb14958e
Dima12411 <fixer40delta@gmail.com>
2021-10-24 19:14:21 +0300

    solution of the task29

---
+++ task29.js
@@ -0,0 +1,8 @@
+/* Task:
+Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator */
+
+//code:
+
+var someAdjective = "awesome!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
