ea07f03d79f5122e82d303f6789d8599b7d84b21
Dima12411 <fixer40delta@gmail.com>
2021-10-24 19:05:44 +0300

    solution of the task26

---
+++ task26.js
@@ -0,0 +1,7 @@
+/* Task:
+Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings. */
+
+//code:
+
+var myStr; // Change this line
+myStr = "This is the start." + " This is the end.";
