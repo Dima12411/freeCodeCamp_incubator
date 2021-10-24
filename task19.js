6f0e463a044f559a0191f253e6f8d1b92a590d78
Dima12411 <fixer40delta@gmail.com>
2021-10-24 18:28:09 +0300

    solution of the task19

---
+++ task19.js
@@ -0,0 +1,13 @@
+/* Task:
+Convert the assignments for a, b, and c to use the -= operator */
+
+//code:
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
