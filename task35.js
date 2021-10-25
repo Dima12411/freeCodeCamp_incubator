aa5234e897d2ec871678abbaf340ba1aa380e94f
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:16:47 +0300

    solution of the task35

---
+++ task35.js
@@ -0,0 +1,10 @@
+/* Task:
+Use bracket notation to find the second-to-last character in the lastName string */
+
+//code:
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
