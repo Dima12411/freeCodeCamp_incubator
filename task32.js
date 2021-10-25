94ad92e81ebb1f55ee6f8ef591afecc58c2a968a
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:09:44 +0300

    solution of the task32

---
+++ task32.js
@@ -0,0 +1,11 @@
+/* Task:
+Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above. */
+
+//code:
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
