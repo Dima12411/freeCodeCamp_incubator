8139642771b1f1c1cc6284773f19d758bdd35ef4
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:32:31 +0300

    solution of the task39

---
+++ task39.js
@@ -0,0 +1,8 @@
+/* Task:
+Create a variable called myData and set it to equal the first value of myArray using bracket notation */
+
+//code:
+
+var myArray = [50,60,70];
+
+var myData = myArray[0];
