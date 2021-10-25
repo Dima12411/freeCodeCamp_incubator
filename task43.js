342d135abbf3132d8aef3a5814352fc002e7e89e
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:42:47 +0300

    solution of the task43

---
+++ task43.js
@@ -0,0 +1,9 @@
+/* Task:
+Use the .pop() function to remove the last item from myArray, assigning the popped off value to removedFromMyArray */
+
+//code:
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+var removedFromMyArray = myArray.pop();
