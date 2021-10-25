0c649c8fc5b3a14b738879110b9c1aa2eaee7ea9
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:25:06 +0300

    solution of the task36

---
+++ task36.js
@@ -0,0 +1,14 @@
+/* Task:
+You will need to use the string concatenation operator + to build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the formed string to the wordBlanks variable. You should not change the words assigned to the variables.
+
+You will also need to account for spaces in your string, so that the final sentence has spaces between all the words. The result should be a complete sentence. */
+
+//code:
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = myNoun + " " + myVerb + " " + myAdjective + " " + myAdverb;
