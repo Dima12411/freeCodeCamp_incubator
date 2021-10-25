676cc036b60bb055620a3f1c6f0b54a3e320c1da
Dima12411 <fixer40delta@gmail.com>
2021-10-25 09:14:26 +0300

    solution of the task34

---
+++ task34.js
@@ -0,0 +1,10 @@
+/* Task:
+Use bracket notation to find the last character in the lastName variable. */
+
+//code:
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length - 1];
