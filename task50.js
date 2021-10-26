c1013e13a888fe9f491bfa9b9021304ff7eec16b
Dima12411 <fixer40delta@gmail.com>
2021-10-26 13:06:46 +0300

    solution of the task50

---
+++ task50.js
@@ -0,0 +1,17 @@
+/* Task:
+The editor has two console.logs to help you see what is happening. Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail */
+
+//code:
+
+function myLocalScope() {
+    var myVar;
+    
+      console.log('inside myLocalScope', myVar);
+    }
+    myLocalScope();
+    
+    // Run and check the console
+    // myVar is not defined outside of myLocalScope
+    console.log('outside myLocalScope', myVar);
