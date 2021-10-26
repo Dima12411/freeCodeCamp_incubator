8efcfaf69f553a2f4c7b6c902def5570316f47c1
Dima12411 <fixer40delta@gmail.com>
2021-10-26 12:52:20 +0300

    solution of the task47

---
+++ task47.js
@@ -0,0 +1,10 @@
+/* Task:
+Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function. */
+
+//code:
+
+function reusableFunction () {
+    console.log("Hi World");
+   }
+   reusableFunction();
