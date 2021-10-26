09e1e2f1ba17af2dfa89f6a1af2fcd8b5de7a1f5
Dima12411 <fixer40delta@gmail.com>
2021-10-26 13:25:03 +0300

    solution of the task54

---
+++ task54.js
@@ -0,0 +1,12 @@
+/* Task:
+Call the processArg function with an argument of 7 and assign its return value to the variable processed. */
+
+//code:
+
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7);
