c64184f5f3f9438f45bd21a9316b142457b29e4d
Dima12411 <fixer40delta@gmail.com>
2021-10-26 12:45:02 +0300

    solution of the task45

---
+++ task45.js
@@ -0,0 +1,8 @@
+/* Task:
+Add ["Paul",35] to the beginning of the myArray variable using unshift() */
+
+//code:
+
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
