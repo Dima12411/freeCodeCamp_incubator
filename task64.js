27b7512c45e0c783a048d43791e18282e6950e3e
Dima12411 <fixer40delta@gmail.com>
2021-10-26 14:51:53 +0300

    solution of the task64

---
+++ task64.js
@@ -0,0 +1,18 @@
+/* Task:
+Add the greater than or equal to operator to the indicated lines so that the return statements make sense */
+
+//code:
+
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  
+      return "20 or Over";
+    }
+  
+    if (val >= 10) {  
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
