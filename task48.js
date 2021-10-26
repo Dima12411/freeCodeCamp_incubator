1a15a77f3613573917e863e17e65c7e8a8476c4f
Dima12411 <fixer40delta@gmail.com>
2021-10-26 12:57:12 +0300

    solution of the task48

---
+++ task48.js
@@ -0,0 +1,11 @@
+/* Task:
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments. */
+
+//code:
+
+function functionWithArgs(a, b) {
+    console.log(a + b);
+   }
+   functionWithArgs(1, 2);
+   functionWithArgs(7, 9);
