82f9c7d27d8ed20d1b90df94f7d577bb6dbac279
Dima12411 <fixer40delta@gmail.com>
2021-10-26 14:42:50 +0300

    solution of the task59

---
+++ task59.js
@@ -0,0 +1,14 @@
+/* Task:
+Use the strict equality operator in the if statement so the function will return the string Equal when val is strictly equal to 7 */
+
+
+//code:
+
+function testStrict(val) {
+    if (val === 7) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
