15e847b1e6d617ac15ff4bf66b8c3aff186ac5ff
Dima12411 <fixer40delta@gmail.com>
2021-10-26 14:36:07 +0300

    solution of the task56

---
+++ task56.js
@@ -0,0 +1,8 @@
+/* Task:
+Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked. */
+
+//code:
+
+function welcomeToBooleans() {
+    return true; 
+  }
