aa17601f7268e54f07a8e653880fd080e12c7692
Dima12411 <fixer40delta@gmail.com>
2021-10-26 14:39:29 +0300

    solution of the task57

---
+++ task57.js
@@ -0,0 +1,12 @@
+/* Task:
+Create an if statement inside the function to return Yes, that was true if the parameter wasThatTrue is true and return No, that was false otherwise. */
+
+//code:
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue == true) {
+      return "Yes, that was true";
+    } else if (wasThatTrue == false) {
+      return "No, that was false"
+    }
+    }
