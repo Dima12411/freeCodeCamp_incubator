36f7f90a80f9dabab3510e31ba2ad3ea9e570d7c
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:40:34 +0300

    solution of the task76

---
+++ task76.js
@@ -0,0 +1,14 @@
+/* Task:
+Delete the tails property from myDog. You may use either dot or bracket notation. */
+
+//code:
+
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  delete myDog.tails;
