402a9c9a4bb5a7cf44a212438df3ae6558e46fff
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:24:16 +0300

    solution of the task71

---
+++ task71.js
@@ -0,0 +1,13 @@
+/* Task:
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt. */
+
+//code:
+
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  var hatValue = testObj.hat;      
+  var shirtValue = testObj.shirt;    
