e03e7ca9e028d682aec515870e333e74657a2f07
Dima12411 <fixer40delta@gmail.com>
2021-10-27 11:07:29 +0300

    solution of the task79

---
+++ task79.js
@@ -0,0 +1,27 @@
+/* Task:
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings. */
+
+//code:
+
+var myMusic = [
+    {
+      "artist": "Twenty One Pilots",
+      "title": "Blueface",
+      "release_year": 2016,
+      "formats": [
+        "CD",
+        "LP"
+      ]
+    },
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
