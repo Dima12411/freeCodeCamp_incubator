fcfdd72105e32025256961f86a59a91bda36fd6b
Dima12411 <fixer40delta@gmail.com>
2021-10-27 13:58:28 +0300

    solution of the task90

---
+++ task90.js
@@ -0,0 +1,10 @@
+/* Task:
+Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax. */
+
+//code:
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
