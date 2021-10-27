26ec0bcbc3ba2858bff423c34d76a57cc27a74b5
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:34:50 +0300

    solution of the task72

---
+++ task72.js
@@ -0,0 +1,13 @@
+/* Task:
+Read the values of the properties an entree and the drink of testObj using bracket notation and assign them to entreeValue and drinkValue respectively. */
+
+//code:
+
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  var entreeValue = testObj["an entree"];   
+  var drinkValue = testObj["the drink"];   
