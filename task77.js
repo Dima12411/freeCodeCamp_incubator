e23d1358cb18238da450a72c1017fddf8f54349b
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:53:11 +0300

    solution of the task77

---
+++ task77.js
@@ -0,0 +1,22 @@
+/* Task:
+Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable. */
+
+//code:
+
+function phoneticLookup(val) {
+    var result = "";
+  
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+    }
+  
+  result = lookup[val];
+    return result;
+  }
+  
+  phoneticLookup("charlie");
