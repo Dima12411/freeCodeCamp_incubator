65b4310b528f05785fbc32294628b0b839a38e6b
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:39:02 +0300

    solution of the task75

---
+++ task75.js
@@ -0,0 +1,13 @@
+/* Task:
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation. */
+
+//code:
+
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+  };
+  
+  myDog.bark = "bow-wow";
