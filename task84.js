a695e2b05784d833539040c50ac126b4c3e9301e
Dima12411 <fixer40delta@gmail.com>
2021-10-27 13:29:23 +0300

    solution of the task84

---
+++ task84.js
@@ -0,0 +1,15 @@
+/* Task:
+Update the code so it only uses the let keyword */
+
+//code:
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
