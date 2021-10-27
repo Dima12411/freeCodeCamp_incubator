b93d9043b16542320087ac01adace475e9f96d9b
Dima12411 <fixer40delta@gmail.com>
2021-10-27 11:28:36 +0300

    solution of the task82

---
+++ task82.js
@@ -0,0 +1,11 @@
+/* Task:
+Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop. */
+
+//code:
+
+var myArray = [];
+var i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
