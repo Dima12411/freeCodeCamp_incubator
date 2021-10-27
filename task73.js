5760186ed4280be85c18f443475735e238ca0f51
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:35:04 +0300

    solution of the task73

---
+++ task73.js
@@ -0,0 +1,13 @@
+/* Task:
+Set the playerNumber variable to 16. Then, use the variable to look up the player's name and assign it to player. */
+
+//code:
+
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  var playerNumber = 16;     
+  var player = testObj[playerNumber];   
