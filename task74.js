bb23e2269864aaf70e6f8048a39f4253bdce52b8
Dima12411 <fixer40delta@gmail.com>
2021-10-27 10:35:20 +0300

    solution of the task74

---
+++ task74.js
@@ -0,0 +1,13 @@
+/* Task:
+Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation. */
+
+//code:
+
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.name = "Happy Coder";
