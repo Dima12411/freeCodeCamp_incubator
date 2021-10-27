1c8bbd9760d6aded04e84b4320a59b8ba3425e50
Dima12411 <fixer40delta@gmail.com>
2021-10-27 14:01:49 +0300

    solution of the task91

---
+++ task91.js
@@ -0,0 +1,6 @@
+/* Task:
+Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified. */
+
+//code:
+
+const increment = (number, value = 1) => number + value;
