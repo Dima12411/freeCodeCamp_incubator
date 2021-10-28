570eb3ba5cde0c99c289650af5e719105899915e
Dima12411 <fixer40delta@gmail.com>
2021-10-28 17:49:56 +0300

    solution of the task95

---
+++ task95.js
@@ -0,0 +1,10 @@
+/* Task:
+Match all the letters in the string quoteSample.
+
+Note: Be sure to match both uppercase and lowercase letters. */
+
+//code:
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex); 
