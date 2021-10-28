cd7fb53445a02d51babe607e9a17c637aa22756c
Dima12411 <fixer40delta@gmail.com>
2021-10-28 18:04:23 +0300

    solution of the task97

---
+++ task97.js
@@ -0,0 +1,8 @@
+/* Task:
+Create a single regex that matches all characters that are not a number or a vowel. Remember to include the appropriate flags in the regex. */
+
+//code:
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/gi; 
+let result = quoteSample.match(myRegex); 
